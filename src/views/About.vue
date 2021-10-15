1338e33dd7fd0fc32d48ca42f5a296e1af87edf3
jonathon <ahnjon77@gmail.com>
2021-10-15 03:23:47 -0700

    adds js libraries hw

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="picker"></div>
   </div>
 </template>
+
+<script>
+/* global iro */
+export default {
+  data: function () {
+    return { pickerColorString: "blahblah" };
+  },
+  mounted: function () {
+    // COLORPICKER (iro.js) STUFF
+    var colorPicker = new iro.ColorPicker("#picker");
+    console.log(colorPicker);
+    colorPicker.on("color:change", function (color) {
+      // log the current color as a HEX string
+      console.log(color.hexString);
+    });
+  },
+  methods: {},
+};
+</script>
